e58ca79255ee2172f050a55ddca738e07895a596
juliop3p <julio15.zn@gmail.com>
2020-10-24 13:43:13 -0300

    frontend template

---
+++ frontend/src/pages/CreatePoint/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function CreatePoint() {
+  return <h1>Formulário criação de ponto</h1>;
+}
+
+export default CreatePoint;
---
+++ frontend/src/pages/Map/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Map() {
+  return <h1>Mapa</h1>;
+}
+
+export default Map;
